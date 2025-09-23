767fe218db797d61ee8f18ca55954ddff7b80ca0
Suprito <a1950138@adelaide.edu.au>
2025-09-23 19:57:09 +0930

    assignment 5

--- ArrMin/ArrMin.asm
+++ ArrMin/ArrMin.asm
@@ -8,28 +8,39 @@ D=M
 @END
 D;JLE          // if R2 <= 0, do nothing (leave R0 unchanged) and halt
 
-(STORE)
+// Make working copies so R1 and R2 remain unchanged
 @R1
+D=M
+@R13
+M=D            // R13 = current pointer (copy of R1)
+
+@R2
+D=M
+@R14
+M=D            // R14 = remaining count (copy of R2)
+
+(STORE)
+@R13
 A=M //get value in R1
 D=M //get first value in array
 
 @R0
 M=D //store in R0
 
-@R1
-M=M+1          // move to second element
+@R13
+M=M+1          // advance working pointer
 
-@R2
-M=M-1          // consumed one element (the first)
+@R14
+M=M-1          // consumed one element from the copy
 
 (LOOP)
-@R2
+@R14
 D=M;
 
 @END
-D;JEQ          // done once we've checked all elements
+D;JEQ
 
-@R1
+@R13
 A=M  //get next array value location
 D=M  //get that array's value
 
@@ -39,19 +50,19 @@ D=D-M //compare with stored
 @NO_UPDATE
 D;JGE          // if current >= stored, skip update
 
-// update stored min = current (don’t touch R2 here)
-@R1
+// update stored min = current (R1/R2 untouched)
+@R13
 A=M
 D=M
 @R0
 M=D
 
 (NO_UPDATE)
-@R1
-M=M+1
+@R13
+M=M+1          // advance working pointer copy
 
-@R2
-M=M-1
+@R14
+M=M-1          // decrement remaining count copy
 
 @LOOP
 0;JMP
