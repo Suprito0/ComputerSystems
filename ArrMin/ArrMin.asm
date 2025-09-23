ecc7eb90907ec22f666e3d2a2b505f4d40cba5b2
Suprito <a1950138@adelaide.edu.au>
2025-09-23 19:38:27 +0930

    assignment 5

--- ArrMin/ArrMin.asm
+++ ArrMin/ArrMin.asm
@@ -3,7 +3,13 @@
 
 // Put your code here.
 
-(STORE)
+// Edge-case guard: empty or non-positive length
+@R2
+D=M
+@END_EMPTY
+D;JLE
+
+(STORE)                 // initial seed of min from first element
 @R1
 A=M //get value in R1
 D=M //get first value in array
@@ -11,8 +17,11 @@ D=M //get first value in array
 @R0
 M=D //store in R0
 
+@R1
+M=M+1           // advance to the second element
+
 @R2
-M=M-1
+M=M-1           
 
 (LOOP)
 @R2
@@ -22,16 +31,25 @@ D=M;
 D;JEQ
 
 @R1
-M=M+1
-
-A=M     //get next array value location
+A=M  //get next array value location
 D=M  //get that array's value
 
 @R0
 D=D-M //compare with stored
 
-@STORE
-D;JLT // Jump to STORE if stored value is greater
+@NO_UPDATE
+D;JGE           // if current >= stored, skip update
+
+// --- Update min without touching R2 here ---
+@R1
+A=M
+D=M
+@R0
+M=D
+
+(NO_UPDATE)
+@R1
+M=M+1
 
 @R2
 M=M-1
@@ -40,5 +58,11 @@ M=M-1
 0;JMP
 
 (END)
+@END
+0;JMP
+
+(END_EMPTY)             // --- Define behavior for empty array ---
+@R0
+M=0                     // choose 0 for "no minimum"
 @END
 0;JMP
--- ArrSort/ArrSort.asm
+++ ArrSort/ArrSort.asm
@@ -4,101 +4,108 @@
 
 
 // Put your code here.
+
 @R2
-M=M+1 
+D=M
+@TRUE
+D;JLE
+
+@R2
+D=M
+@passLen
+M=D
+@passLen
+M=M-1
 
 (LOOPO)
-    @R2
-    M=M-1
-    D=M //assiging the length of array to D
+@passLen
+D=M
+@TRUE
+D;JLT          // when passLen < 0, all passes done
+@R2
+D=M //assiging the length of array to D          
 
-    @count
-    M=D  // assigning the length of array to the variable count
+@count
+M=D  // assigning the length of array to the variable count   
+@count
+M=M-1              // we only compare pairs, so count = R2 - 1 for inner loop
 
-    @R1
-    D=M // assigning the index of array to D
+@R1
+D=M // assigning the index of array to D                       
 
-    @index
-    M=D // assigning the index of array to the variable index
+@index
+M=D // assigning the index of array to the variable index      
 
-    (LOOPI)
-        @index
-        D=M
-        A=D // @index
-        D=M // assigning value of array to D
+(LOOPI)
+@count
+D=M
+@AFTER_INNER
+D;JLT              // when count < 0, inner loop finished
 
-        A=A+1   //@ next index
-        D=D-M   // compare the two index
+@index
+D=M
+A=D // @index                                            
+D=M // assigning value of array to D                      
 
-        @SWITCH
-        D;JGT   //if >0 jump
+A=A+1   //@ next index                                   
+D=D-M   // compare the two index                          
 
-        @index
-        M=M+1
+@SWITCH
+D;JGT   //if >0 jump                                      
 
-        @count
-        M=M-1
-        D=M
+@index
+M=M+1
 
-        @LOOPO
-        D-1;JEQ
+@count
+M=M-1
+D=M
 
-        @R2
-        D=M
-
-        @TRUE
-        D-1;JEQ
-
-        @LOOPI
-        0;JMP
+@LOOPI
+0;JMP
 
 (SWITCH)
-    @index
-    D=M     //get index
+@index
+D=M     //get index                                           
 
-    A=D  // go to index
-    D=M //store specific value
+A=D  // go to index                                           
+D=M //store specific value                                    
 
-    @temp  
-    M=D  //store the value in the variable temp
+@temp  
+M=D  //store the value in the variable temp                   
 
-    @index 
-    A=M+1   //go to address of the next index
-    D=M     //store value of that index
+@index 
+A=M+1   //go to address of the next index                     
+D=M     //store value of that index                           
 
-    @index
-    A=M     // go to the original index
-    M=D     //assign the next index here
+@index
+A=M     // go to the original index                           
+M=D     //assign the next index here                          
 
-    @temp   
-    D=M     // go to temp to get the value of the original index
+@temp   
+D=M     // go to temp to get the value of the original index  
 
-    @index
-    M=M+1   // increment index
+@index
+M=M+1   // increment index                                    
 
-    A=M     // go to that address
-    M=D     // set the value of the original index here 
-            //switching complete
+A=M     // go to that address                                 
+M=D     // set the value of the original index here       
 
-    @count
-    M=M-1
-    D=M
+@count
+M=M-1
+D=M
 
-    @LOOPO
-    D-1;JEQ
+@LOOPI
+0;JMP
 
-    @R2
-    D=M
-
-    @TRUE
-    D-1;JEQ
-
-    @LOOPI
-    0;JMP
+(AFTER_INNER)
+@passLen
+M=M-1
+@LOOPO
+0;JMP
 
 (TRUE)
 @R0
-M=!A
+M=-1          // M=!A (original) could be brittle; set True (-1) directly
 
 (END)
 @END
